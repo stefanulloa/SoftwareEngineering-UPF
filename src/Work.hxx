54dbdd2b7000a428647ec7c206e613db1d0789b3
u124317 <u124317@54021p10u.ds.upf.edu>
2017-02-08 18:12:08 +0100

    GREEN: ItemTests::testTitle_addingTitle

--- src/Work.hxx
+++ src/Work.hxx
@@ -1,15 +1,25 @@
-
 #ifndef _WORK_HXX_
 #define _WORK_HXX_
 
+//si aqui se pone "using namespace std", ya no hara falta poner std en el resto de sitios
+
 class Work {
-	
-	public: 
-		std::string title() const {
-			return "-- Untitled --";
+
+private: std::string _title;
+
+public:
+	Work() //metodo constructor, mismo nombre de la clase
+		: _title ("-- Untitled --") // manera de declarar valor para no hacerlo dos veces
+	{
+				//si se inicializase aqui, como en java, haria trabajo doble
+	} 
+
+		const std::string title() const { //si se pone const quiere decir que desde fuera no se puede cambiar el valor 
+			return _title;
 		}
 		
 		void title( const std::string &title) {
+			_title = title;
 		}
 };
 
