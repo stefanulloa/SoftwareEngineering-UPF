b36df26d05f79739b5e96407cb25019661656997
u124317 <u124317@54021p19u.ds.upf.edu>
2017-02-08 12:16:34 +0100

    Tutorial P1 Version 3

--- tutorial/exemple_polimorfisme.cxx
+++ tutorial/exemple_polimorfisme.cxx
@@ -1,12 +1,12 @@
 /* 
-  - definició d'una classe
-  - instaciacio i missatge a un objecte
+  - classe acabada en ;
+  - mètode public:
  */
 #include <iostream>
 
 class Animal
 {
-	public:
+public:
 	void escriuEspecie()
 	{
 		std::cout << "...indefinida..." << std::endl;
@@ -20,10 +20,3 @@ int main(void)
 
 	return 0;
 }
-
-/*
-   Problemes: 
-   no ens compila per dos detalls importants:
-   - la declaració d'una classe en C++ ha d'acabar en ';'
-   - per defecte, els mètodes i atributs són privats. Cal fer-lo públic.
- */
