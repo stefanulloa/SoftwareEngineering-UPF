014cde311cf62fec4b5670858609de8121d683b7
u124317 <u124317@54006p23.aulesca.upf.edu>
2017-02-21 21:46:23 +0100

    RED: ItemTests::testName_byDefault

---
+++ src/Author.hxx
@@ -0,0 +1,15 @@
+#ifndef _AUTHOR_HXX_
+#define _AUTHOR_HXX_
+
+class Author{
+
+public:
+	
+	
+	const std::string getName() const {
+		return "Antonio";
+	}	
+	
+};
+
+#endif
---
+++ src/unitTests/AuthorTests.cxx
@@ -0,0 +1,19 @@
+#include "MiniCppUnit.hxx"
+#include "Author.hxx"
+
+class AuthorTests : public TestFixture<AuthorTests>
+{
+public:
+	TEST_FIXTURE( AuthorTests )
+	{
+		TEST_CASE( testName_byDefault );	
+	}
+	void testName_byDefault()
+	{
+		Author author;
+		ASSERT_EQUALS( "Pedro", author.getName() );
+	}
+
+};
+
+REGISTER_FIXTURE( AuthorTests )
