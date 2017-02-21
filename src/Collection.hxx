4dbd70279fc0203cb246b58e15a08a6450cefdce
u124317 <u124317@54006p23.aulesca.upf.edu>
2017-02-21 21:06:12 +0100

    RED: ItemTests::testTitle_byDefault Collection

---
+++ src/Collection.hxx
@@ -0,0 +1,13 @@
+#ifndef _COLLECTION_HXX_
+#define _COLLECTION_HXX_
+
+class Collection{
+
+public:
+
+	const std::string title() const {
+		return "hola";
+	}
+};
+
+#endif
--- src/unitTests/CollectionTests.cxx
+++ src/unitTests/CollectionTests.cxx
@@ -1,22 +1,23 @@
 #include "MiniCppUnit.hxx"
+#include "Collection.hxx"
 
 class CollectionTests : public TestFixture<CollectionTests>
 {
 public:
 	TEST_FIXTURE( CollectionTests )
 	{
-//		TEST_CASE( testTitle_byDefault );
+		TEST_CASE( testTitle_byDefault );
 //		TEST_CASE( testTitle_addingTitle );
 //		TEST_CASE( testPublished_byDefault );
 //		TEST_CASE( testPublished_afterPublishing );
 //		TEST_CASE( testPublished_afterWithdrawing );
 	}
-/*	void testTitle_byDefault()
+	void testTitle_byDefault()
 	{
 		Collection collection;
 		ASSERT_EQUALS( "-- Untitled --", collection.title() );
 	}
-	void testTitle_addingTitle()
+	/*void testTitle_addingTitle()
 	{
 		Collection collection;
 		collection.title( "A Title" );
