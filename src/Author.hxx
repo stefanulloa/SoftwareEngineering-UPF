b08f5bc9725a2ea649e6939b5ec6618826fdcf1b
u124317 <u124317@54009p10.aulesca.upf.edu>
2017-02-22 12:57:33 +0100

    RED: ItemTests::testName_addingName

--- src/Author.hxx
+++ src/Author.hxx
@@ -2,13 +2,20 @@
 #define _AUTHOR_HXX_
 
 class Author{
+	private: std::string _name;
 
 public:
-	
-	
+	Author()
+	:_name ("Pedro")
+	{
+	}
+		
 	const std::string getName() const {
-		return "Pedro";
+		return _name;
 	}	
+	void setName(const std::string &name){
+	
+	}
 	
 };
 
--- src/unitTests/AuthorTests.cxx
+++ src/unitTests/AuthorTests.cxx
@@ -7,12 +7,19 @@ public:
 	TEST_FIXTURE( AuthorTests )
 	{
 		TEST_CASE( testName_byDefault );
+		TEST_CASE( testName_addingName );
 	
 	}
 	void testName_byDefault()
 	{
 		Author author;
 		ASSERT_EQUALS( "Pedro", author.getName() );
+	}
+	void testName_addingName()
+	{
+		Author author;
+		author.setName("Antonio");
+		ASSERT_EQUALS( "Antonio", author.getName() );
 	}
 
 	
