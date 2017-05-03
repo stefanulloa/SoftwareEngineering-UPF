f6362f5ac18ca03d88880b9049f5820e8889d571
u124317 <u124317@54021p19u.ds.upf.edu>
2017-05-03 12:24:13 +0200

    RED::testConvert_withHtmlConverter

---
+++ src/ConverterGroup.hxx
@@ -0,0 +1,34 @@
+#ifndef _CONVERTERGROUP_HXX_
+#define _CONVERTERGROUP_HXX_
+#include "Converter.hxx"
+#include "HtmlConverter.hxx"
+#include "PdfConverter.hxx"
+
+class ConverterGroup {
+	
+	protected:
+	HtmlConverter _oneHtmlConverter;
+	
+	public:
+		ConverterGroup()
+		{	
+		} 
+
+		~ConverterGroup()
+		{
+		}	
+		
+		void convert(const std::string &originalFileName, const std::string &generatedFileName){
+			_oneHtmlConverter.convert(originalFileName, originalFileName);
+			};
+		
+		void add(const std::string type){
+			if(type == "html"){
+				HtmlConverter convert;
+				_oneHtmlConverter = convert;
+			}
+		};
+		
+};
+
+#endif
---
+++ src/unitTests/ConverterGroupTests.cxx
@@ -0,0 +1,60 @@
+#include <fstream>
+#include "LibFileSystem.hxx"
+#include "MiniCppUnit.hxx"
+#include "Converter.hxx"
+#include "ConverterGroup.hxx"
+
+class ConverterGroupTests : public TestFixture<ConverterGroupTests>
+{
+public:
+	TEST_FIXTURE( ConverterGroupTests )
+	{
+		TEST_CASE( testConvert_withHtmlConverter );
+	}
+
+	/**
+	 * The setUp method is automatically called before each test
+	 */
+	void setUp()
+	{
+		LibFileSystem::createNetDirectory( "config" );
+		LibFileSystem::createNetDirectory( "generated" );
+		LibFileSystem::createNetDirectory( "originals" );
+	}
+	/**
+	 * The tearDown method is automatically called after each test
+	 */
+	void tearDown()
+	{
+		LibFileSystem::cleanupDirectory( "config" );
+		LibFileSystem::cleanupDirectory( "generated" );
+		LibFileSystem::cleanupDirectory( "originals" );
+	}
+	/**
+	 * This method creates files used in tests
+	 */
+	void createOriginalFile( const std::string & name )
+	{
+		std::string fullname( "originals/" );
+		fullname += name;
+		std::ofstream os( fullname.c_str() );
+		os << "An original file" << std::endl;
+		os.close();
+	} 
+	
+	void testConvert_withHtmlConverter()
+	{
+		ConverterGroup converter;
+		createOriginalFile( "Original.odt" );
+		converter.add("html");
+		converter.convert( "originals/Original.odt", "generated/Prefix" );
+
+		ASSERT_EQUALS(
+			"generated/Prefix [multiple HTML files].war\n", 
+			LibFileSystem::listDirectoryInOrder( "generated" ) 
+		); 
+	}
+	
+};
+
+REGISTER_FIXTURE( ConverterGroupTests )
